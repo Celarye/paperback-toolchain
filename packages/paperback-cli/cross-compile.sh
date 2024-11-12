ba9780d26dec155332b1ce0631fb36aabd3a6fd0
Faizan Durrani <faizandurrani324@gmail.com>
2024-11-12 18:44:34 +0500

    npm experiments

--- packages/paperback-cli/cross-compile.sh
+++ packages/paperback-cli/cross-compile.sh
@@ -1,4 +1,4 @@
-env GOOS=linux GOARCH=amd64 go build -o ./bin/paperback-cli_linux_amd64 -ldflags "-w"
-env GOOS=windows GOARCH=amd64 go build -o ./bin/paperback-cli_windows_amd64 -ldflags "-w"
-env GOOS=darwin GOARCH=amd64 go build -o ./bin/paperback-cli_darwin_amd64 -ldflags "-w"
-env GOOS=darwin GOARCH=arm64 go build -o ./bin/paperback-cli_darwin_arm64 -ldflags "-w"
+env GOOS=linux GOARCH=amd64 go build -o ../paperback-cli-linux-amd64/bin/paperback-cli -ldflags "-w"
+env GOOS=windows GOARCH=amd64 go build -o ../paperback-cli-windows-amd64/bin/paperback-cli.exe -ldflags "-w"
+env GOOS=darwin GOARCH=amd64 go build -o ../paperback-cli-darwin-amd64/bin/paperback-cli -ldflags "-w"
+env GOOS=darwin GOARCH=arm64 go build -o ../paperback-cli-darwin-arm64/bin/paperback-cli -ldflags "-w"
